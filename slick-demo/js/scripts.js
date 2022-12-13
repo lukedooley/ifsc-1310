53f908cb34edd000be6acc491f51d6d99fffef4a
lukedooley <ldooley@ualr.edu>
2022-12-13 21:46:16 +0000

    progressive enhancement and slick

---
+++ slick-demo/js/scripts.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+    $('.multiple-items').slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 1000
+    });
+
+
+});
